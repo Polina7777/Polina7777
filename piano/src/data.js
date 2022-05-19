2216bc441580508f76680a7985c761ad52fde925
polina <polina@mail.ru>
2022-05-19 16:47:54 +0300

    piano key

---
+++ piano/src/data.js
@@ -0,0 +1,123 @@
+
+export const keys = [
+
+    {
+        "type": "white",
+        "title": {
+            "ru": "ДО",
+            "en": "C"
+        },
+        "audio": " ",
+        "keyButton": "KeyQ"
+    },
+    {
+        "type": "black",
+        "title": {
+            "ru": "B1",
+            "en": "B1"
+        },
+        "audio": "",
+        "keyButton": "Digit2"
+    },
+    {
+        "type": "white",
+        "title": {
+            "ru": "РЕ",
+            "en": "D"
+        },
+        "audio": "",
+        "keyButton": "KeyW"
+    },
+    {
+        "type": "black",
+        "title": {
+            "ru": "B2",
+            "en": "B2"
+        },
+        "audio": "",
+        "keyButton": "Digit3"
+    },
+    {
+        "type": "white",
+        "title": {
+            "ru": "МИ",
+            "en": "E"
+        },
+        "audio": "",
+        "keyButton": "KeyE"
+    },
+    {
+        "type": "white",
+        "title": {
+            "ru": "ФА",
+            "en": "F"
+        },
+        "audio": "",
+        "keyButton": "KeyR"
+    },
+    {
+        "type": "black",
+        "title": {
+            "ru": "B3",
+            "en": "B3"
+        },
+        "audio": "",
+        "keyButton": "Digit5"
+    },
+    {
+        "type": "white",
+        "title": {
+            "ru": "СОЛЬ",
+            "en": "G"
+        },
+        "audio": "",
+        "keyButton": "KeyT"
+    },
+    {
+        "type": "black",
+        "title": {
+            "ru": "B4",
+            "en": "B4"
+        },
+        "audio": "",
+        "keyButton": "Digit6"
+    },
+    {
+        "type": "white",
+        "title": {
+            "ru": "ЛЯ",
+            "en": "A"
+        },
+        "audio": "",
+        "keyButton": "KeyY"
+    },
+    {
+        "type": "black",
+        "title": {
+            "ru": "B5",
+            "en": "B5"
+        },
+        "audio": "",
+        "keyButton": "Digit7"
+    },
+    {
+        "type": "white",
+        "title": {
+            "ru": "СИ",
+            "en": "B"
+        },
+        "audio": "",
+        "keyButton": "KeyU"
+    },
+    {
+        "type": "white",
+        "title": {
+            "ru": "ДО",
+            "en": "C"
+        },
+        "audio": "",
+        "keyButton": "KeyI"
+    }
+
+
+]
